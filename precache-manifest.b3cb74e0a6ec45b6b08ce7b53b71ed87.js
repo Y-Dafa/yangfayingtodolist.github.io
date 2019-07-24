0d455746f52cc39bcb0c83a25c600845854b7938
25522 <FAYING.YANG@HAND-CHINA.COMM>
2019-07-25 00:50:27 +0800

    todolist

---
+++ precache-manifest.b3cb74e0a6ec45b6b08ce7b53b71ed87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c27427b89620c199058d5f82783e80f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d222cbd793d9c21c83b8",
+    "url": "/static/css/main.c12b480b.chunk.css"
+  },
+  {
+    "revision": "64bf822d7dc41a914032",
+    "url": "/static/js/2.96dd3660.chunk.js"
+  },
+  {
+    "revision": "d222cbd793d9c21c83b8",
+    "url": "/static/js/main.b612219d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
